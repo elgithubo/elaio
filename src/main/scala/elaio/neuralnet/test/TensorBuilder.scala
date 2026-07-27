8a6d1517bf483c4b318597560f3aec1bef68fa25
Tino Engel <tino.engel@mail.de>
2026-07-27 22:13:56 +0200

    comment change only

--- src/main/scala/elaio/neuralnet/test/TensorBuilder.scala
+++ src/main/scala/elaio/neuralnet/test/TensorBuilder.scala
@@ -20,7 +20,7 @@ object TensorBuilder {
 
   // run the test case
   def run(): Unit = {
-    // Enable the following line to write detailed trace messages to stdout
+    // Enable the following line to write detailed trace messages to stdout, disable it for no output.
     NetTrace.started_(true)
 
     NetTrace.WriteMessage("start of test run")
