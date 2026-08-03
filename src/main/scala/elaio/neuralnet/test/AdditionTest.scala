3bc5d8ea6f991bcf2149dbe8646bb3ac2bd4f51c
Tino Engel <tino.engel@mail.de>
2026-08-03 19:13:31 +0200

    adjusted learning rates from measurement results

--- src/main/scala/elaio/neuralnet/test/AdditionTest.scala
+++ src/main/scala/elaio/neuralnet/test/AdditionTest.scala
@@ -4,6 +4,6 @@ import elaio.neuralnet.persistence.PersistenceAction
 
 final class AdditionTest(override protected val persistenceAction: Option[PersistenceAction] = None)
     extends MathTest {
-  override protected val learningRate = 0.003
+  override protected val learningRate = 0.01
   protected def targetOf(inputValues: Array[Double]): Array[Double] = inputValues.map(value => value + 23)
 }
