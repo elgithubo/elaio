2a222e6bc5c0217c17516b5ff310a894acbc519f
Tino Engel <tino.engel@mail.de>
2026-08-04 07:09:00 +0200

    updated learning rate and model files for addition and subtraction

--- src/main/scala/elaio/neuralnet/test/AdditionTest.scala
+++ src/main/scala/elaio/neuralnet/test/AdditionTest.scala
@@ -4,7 +4,7 @@ import elaio.neuralnet.persistence.PersistenceAction
 
 final class AdditionTest(override protected val persistenceAction: Option[PersistenceAction] = None)
     extends MathTest {
-  override protected val learningRate = 0.003d
+  override protected val learningRate = 0.005d
   override protected val maxUpdateNorm = 1000d
 
   protected def targetOf(inputValues: Array[Double]): Array[Double] = inputValues.map(value => value + 23)
