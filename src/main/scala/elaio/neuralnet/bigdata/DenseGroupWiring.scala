67339a1290b036c8d4f8968da2afe3b2bf7b14d2
Tino Engel <tino.engel@mail.de>
2026-08-07 17:53:27 +0200

    do not connect inputs to outputs

--- src/main/scala/elaio/neuralnet/bigdata/DenseGroupWiring.scala
+++ src/main/scala/elaio/neuralnet/bigdata/DenseGroupWiring.scala
@@ -1,10 +1,12 @@
 package elaio.neuralnet.bigdata
 
-import elaio.neuralnet.units.{HiddenNeuronSquare, Neuron}
+import elaio.neuralnet.units.{HiddenNeuronSquare, InputNeuron, Neuron, OutputNeuron}
 
 object DenseGroupWiring extends AdditionalWiring {
   private def connectsSquareNeurons(source: Neuron, target: Neuron): Boolean =
     source.isInstanceOf[HiddenNeuronSquare] && target.isInstanceOf[HiddenNeuronSquare]
+  private def connectsInputToOutput(source: Neuron, target: Neuron): Boolean =
+    source.isInstanceOf[InputNeuron] && target.isInstanceOf[OutputNeuron]
 
   override def wire(context: AdditionalWiring.Context): Unit =
     for {
@@ -12,6 +14,6 @@ object DenseGroupWiring extends AdditionalWiring {
       targetGroupIndex <- sourceGroupIndex + 1 until context.groups.length
       source <- context.groups(sourceGroupIndex).neurons
       target <- context.groups(targetGroupIndex).neurons
-      if !connectsSquareNeurons(source, target)
+      if !connectsSquareNeurons(source, target) && !connectsInputToOutput(source, target)
     } context.connect(source, target)
 }
